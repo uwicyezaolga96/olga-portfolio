eef999f82b32b9a27809343f9b331487e1354c1c
olgauwicyeza96 <olgauwicyeza184@gmail.com>
2024-07-09 08:05:16 -0700

    diploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  },
-  output: 'server',
-  adapter: vercel({
-    imageService: true,
-  }),
+  integrations: [react(), tailwind()],  
 });
